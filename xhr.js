b3fd9350ca1f543db0f87269edcba521bc656e63
marufbellah <mdmaruf5511507@gmail.com>
2023-11-25 20:25:09 +0600

    xhr get is done

--- xhr.js
+++ xhr.js
@@ -1,1 +1,31 @@
 console.clear();
+
+// const makeRequest = (method, url, data) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open(method, url);
+//   xhr.send(JSON.stringify(data));
+// };
+
+
+
+// bar bar korar jonno evabe kore ================
+
+
+// GET --- evabe muloto kore ========================> 
+
+const getData = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+
+  xhr.onload = () => {
+    let data = xhr.response;
+    console.log(data);
+  };
+
+  xhr.onerror = () => {
+    console.log("error is here");
+  };
+  xhr.send();
+};
+
+getData();
